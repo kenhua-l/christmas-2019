2b2d5a5b07960b504ee7fe23f3d51d71ff8aa420
kenhua <ken.liew@websparks.sg>
2019-12-15 12:47:24 +0800

    minimise css and modify js

--- assets/main.js
+++ assets/main.js
@@ -3,29 +3,19 @@ window.addEventListener('load', function(){
   var musicButton = document.querySelector('[aria-label="music"]');
   music.load();
   var playPromise = music.play();
-  if (playPromise !== undefined) {
-    playPromise.then(function() {
-      //autoplay
-      musicButton.classList.add('pause');
-      musicButton.classList.remove('play');
-    }).catch(function(){
-      musicButton.classList.add('play');
-      musicButton.classList.remove('pause');
-    });
-  } else {
-    musicButton.classList.add('pause');
-    musicButton.classList.remove('play');
+  if(playPromise !== undefined){
+    playPromise.then(changeMusicButtonIcon(musicButton, 'pause', 'play')).catch(changeMusicButtonIcon(musicButton, 'play', 'pause'));
+  }else{
+    changeMusicButtonIcon(musicButton, 'pause', 'play');
   }
-
   musicButton.onclick = function(){
     if (musicButton.classList.contains('play')) {
       music.play();
-      musicButton.classList.add('pause');
-      musicButton.classList.remove('play');
+      changeMusicButtonIcon(musicButton, 'pause', 'play');
     } else {
       music.pause();
-      musicButton.classList.add('play');
-      musicButton.classList.remove('pause');
+      changeMusicButtonIcon(musicButton, 'play', 'pause');
     }
   };
 });
+function changeMusicButtonIcon(button, add, remove){button.classList.add(add);button.classList.remove(remove);}
